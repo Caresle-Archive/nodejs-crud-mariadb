449f2ebcf811fc0f6f8671161fe9839baaa25d08
Caresle <carabesle@gmail.com>
2021-11-01 18:55:12 -0700

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -7,12 +7,20 @@ const path = require('path')
 const PORT = process.env.PORT || 3001
 
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
+
+// View engine
 app.set('views', path.join(__dirname, 'views'))
 app.engine('hbs', exphbs({
 	defaultLayout: 'main',
-	extname: '.hbs',
 	layoutsDir: path.join(app.get('views'), 'layouts'),
-	partialsDir: path.join(app.get('views'), 'partials')
+	partialsDir: path.join(app.get('views'), 'partials'),
+	extname: '.hbs'
 }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+	res.render('index')
+})
 
 app.listen(PORT, () => console.log(`Server on PORT ${PORT}`))
